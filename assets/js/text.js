8784bfa315c6b5a6845701583bbbb12030c056e2
remzi <remzi.github@gmail.com>
2022-07-11 18:07:51 +0500

    first commit

---
+++ assets/js/text.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".typed").typed({
+        strings: ["Крым мой", "#НетВойне", "ANTI-SCAMMMER", "Краш", "coder", "Remzi_pro_Mortis" , "vzlom jopi", "hmmm...", "bruh", "Hack system...", "h@Cк3D!"],
+        typeSpeed: 30,
+        backSpeed: 0,
+        cursorChar: ['❚'],
+        smartBackspace: true,
+        fadeOut: true,
+        loop: true,
+    });
+});
